eb48b7dc4f14185ecc93b41b184024486994fc8e
min <67621261+mino-8136@users.noreply.github.com>
2024-06-30 15:50:25 +0900

    ダイアログの見栄え調整

--- src/components/ProblemsTable.vue
+++ src/components/ProblemsTable.vue
@@ -28,29 +28,36 @@
   </v-table>
 
   <v-dialog v-model="dialog" width="auto" min-width="400px">
-    <v-card
-      prepend-icon="mdi-update"
-      title="実行結果"
-    >
+    <v-card>
+      <v-card-title :class="selectedResult?.status ? 'success-title' : 'error-title'" class="py-4">
+        <v-icon
+          :icon="selectedResult?.status ? 'mdi-check-bold' : 'mdi-exclamation-thick'"
+          class="me-2"
+        ></v-icon>
+        {{ selectedResult?.status ? '正解です' : '結果の見直しが必要です' }}
+      </v-card-title>
       <v-card-text>
         <div v-if="selectedResult">
-          <p><strong>入力例:</strong></p><p>{{ selectedResult.input }}</p>
-          <p><strong>出力例:</strong></p><p>{{ selectedResult.received_output }}</p>
-          <p><strong>あなたの実行結果:</strong></p><p>{{selectedResult.received_output }}</p>
+          <p><strong>入力例:</strong></p>
+          <p class="mb-2">{{ selectedResult.input }}</p>
+          <p><strong>出力例:</strong></p>
+          <p class="mb-2">{{ selectedResult.expected_output }}</p>
+          <p><strong>あなたの実行結果:</strong></p>
+          <p class="mb-2">{{ selectedResult.received_output }}</p>
         </div>
       </v-card-text>
+      <v-divider></v-divider>
       <template v-slot:actions>
         <v-btn class="ms-auto" @click="dialog = false">Ok</v-btn>
       </template>
     </v-card>
   </v-dialog>
-
 </template>
 
 <script setup lang="ts">
 import { useAssignmentStore } from '../stores/assignmentStore'
-import { Results } from '@/assets/Commons';
-import { ref } from 'vue';
+import { Results } from '@/assets/Commons'
+import { ref } from 'vue'
 
 const store = useAssignmentStore()
 const dialog = ref(false)
@@ -76,5 +83,15 @@ function openDialog(result: Results) {
   display: flex;
   flex-wrap: wrap;
   gap: 8px; /* チップ間のスペースを設定 */
+}
+
+.success-title {
+  background-color: #4caf50; /* 背景色を緑に設定 */
+  color: white;
+}
+
+.error-title {
+  background-color: #f44336; /* 赤色の背景 */
+  color: white;
 }
 </style>
