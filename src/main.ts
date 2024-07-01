1ada5a13580a1bd061655c80befde2726d846e77
min <67621261+mino-8136@users.noreply.github.com>
2024-07-01 20:05:25 +0900

    コロン

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ const vuetify = createVuetify({
         colors: {
           primary: '#1976d2',
           correct: '#4CAF50',
-          error: '#FF5252'
+          error: '#FF5252',
         }
       }
     }
