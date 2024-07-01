2183b21da017462e87fc110ea36c00dde929719a
min <67621261+mino-8136@users.noreply.github.com>
2024-07-01 19:41:06 +0900

    色定義の変更とコメント削除

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,6 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import "@mdi/font/css/materialdesignicons.css";
 
-// Axios
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
 const vuetify = createVuetify({
   components,
   directives,
@@ -26,14 +22,18 @@ const vuetify = createVuetify({
       light: {
         colors: {
           primary: '#1976d2',
+          correct: '#4CAF50',
+          incorrect: '#FF5252',
+          error: '#FFC107',
         }
       }
     }
   }
 })
 
-// Electron
-
+// Axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 const app = createApp(App)
 
