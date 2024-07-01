28d84c8f19174ed1453c7fc17983dca76b0164c0
min <67621261+mino-8136@users.noreply.github.com>
2024-07-01 20:36:22 +0900

    git管理ファイルの更新

--- src/components/AssignmentDescription.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h2 class="my-4 text-center" >{{ store.selectedAssignment.title }}</h2>
-  <p class="text-center"> {{ store.selectedAssignment.description }}</p>
-</template>
-
-<script setup lang="ts">
-import { useAssignmentStore } from '../stores/assignmentStore'
-const store = useAssignmentStore() // storeのインスタンスを取得
-</script>
