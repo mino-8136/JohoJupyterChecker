964a3eea3cffdff2ea7439ebbdd8f1e19562d479
min <67621261+mino-8136@users.noreply.github.com>
2024-11-15 09:20:29 +0900

    暗号化処理のシンプル化

--- src/components/StudentScore.vue
+++ src/components/StudentScore.vue
@@ -5,7 +5,12 @@
     <v-card-text class="mx-4">
       <v-row align="center" justify="center" class="pb-4">
         <v-col cols="4">
-          <v-progress-circular :model-value="(currentScore / totalScore) * 100" :size="120" :width="15" color="primary">
+          <v-progress-circular
+            :model-value="(currentScore / totalScore) * 100"
+            :size="120"
+            :width="15"
+            color="primary"
+          >
             <div class="score">
               <span class="number">{{ Math.floor((currentScore / totalScore) * 100) }}</span>
               <span class="percent">%</span>
@@ -19,9 +24,22 @@
       <v-divider></v-divider>
     </v-card-text>
     <v-card-text align="center" class="px-8">
-      <v-text-field label="学年組番号の4桁" v-model="studentId" density="comfortable" variant="outlined" width="85%" :rules="[rules.number]">
+      <v-text-field
+        label="学年組番号の4桁"
+        v-model="studentId"
+        density="comfortable"
+        variant="outlined"
+        width="85%"
+        :rules="[rules.number]"
+      >
         <template v-slot:append>
-          <v-btn color="primary" @click="copyResult" class="rounded-pill" :disabled="!isStudentIdValid">提出用に結果をコピー</v-btn>
+          <v-btn
+            color="primary"
+            @click="copyResult"
+            class="rounded-pill"
+            :disabled="!isStudentIdValid"
+            >提出用に結果をコピー</v-btn
+          >
         </template>
       </v-text-field>
     </v-card-text>
@@ -35,7 +53,7 @@
       </v-card-title>
       <v-card-text>
         <p>データがクリップボードにコピーされました。</p>
-        <p>このままPC室のFormリンクか、<br>Teamsの課題に貼り付けて提出してください。</p>
+        <p>このままPC室のFormリンクか、<br />Teamsの課題に貼り付けて提出してください。</p>
       </v-card-text>
       <v-divider></v-divider>
       <template v-slot:actions>
@@ -50,6 +68,12 @@ import { ref, computed } from 'vue'
 import { useAssignmentStore } from '../stores/assignmentStore'
 import CryptoJS from 'crypto-js'
 
+////////////////////////////////////////////////////////////////
+
+const encryptionKey = `kadaiJudge` // AES暗号化のキー
+
+////////////////////////////////////////////////////////////////
+
 const store = useAssignmentStore()
 const totalScore = computed(() => store.getTotalScore)
 const currentScore = computed(() => store.getCurrentScore)
@@ -57,7 +81,7 @@ const dialog = ref(false)
 
 const studentId = ref('')
 const rules = {
-  number: (v: string) => v === '' || /^\d{4}$/.test(v) || '4桁の半角数字で入力',
+  number: (v: string) => v === '' || /^\d{4}$/.test(v) || '4桁の半角数字で入力'
 }
 const isStudentIdValid = computed(() => {
   return /^\d{4}$/.test(studentId.value)
@@ -84,31 +108,35 @@ function copyResult() {
     return
   }
 
-  const assignmentData = store.selectedAssignment;
-  const assignmentTitle = assignmentData.title;
-  const score = currentScore.value;
+  const assignmentTitle = store.selectedAssignment.title
+  const score = currentScore.value
 
-  // 暫定的に、文字列とassignmentTitleと入力IDとスコアをアンダーバーで繋いだものをキーとして使用
-  const encryptionKey = `kadaiJudge_${assignmentTitle}_${studentId.value}_${score}`;
-  
-  // 入力IDとスコアをキーとしてstore.selectedAssignmentを暗号化(TODO : バックエンドへ移す)
-  const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(assignmentData), encryptionKey).toString();
-  
+  // 暗号化キーを用いて課題得点情報を暗号化(TODO : バックエンドへ移す)
+  // 検出できる不正 : 課題名を書き換えた・スコアを書き換えた・他人の学年組番号を入力して提出した
+  const encryptedData = CryptoJS.AES.encrypt(
+    JSON.stringify({
+      assignmentTitle: assignmentTitle,
+      studentId: studentId.value,
+      score: score
+    }),
+    encryptionKey
+  ).toString()
+
   const result = {
     assignmentTitle: assignmentTitle,
     studentId: studentId.value,
     score: score,
     encryptedData: encryptedData
-  };
-  
-  navigator.clipboard.writeText(JSON.stringify(result)).then(() => {
-    dialog.value = true;
-  }).catch(err => {
-    console.error('コピーに失敗しました:', err);
-  });
-  
-  // 検出できる不正 : 別の課題で実行したスコアを提出した・スコアを書き換えた・他人の学年組番号を入力して提出した
-  // テスト用キー例 : kadaiJudge_①Pythonの基本_1234_240
+  }
+
+  navigator.clipboard
+    .writeText(JSON.stringify(result))
+    .then(() => {
+      dialog.value = true
+    })
+    .catch((err) => {
+      console.error('コピーに失敗しました:', err)
+    })
 }
 </script>
 
